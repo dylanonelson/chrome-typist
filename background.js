fce10f43985d7cc4ad63fde2ed57b95e874587bb
Dylan Nelson <Dylan.Nelson@nytimes.com>
2016-05-07 15:46:31 -0400

    Log 'Yo' when the user triggers the vim-click command

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.commands.onCommand.addListener(function(command) {
+	chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			message: 'Yo'
+		});
+	});
+})
