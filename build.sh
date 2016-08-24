0d32a85951d25deffd6aaec40f5f8c75cebf6858
Dylan Nelson <Dylan.Nelson@nytimes.com>
2016-08-23 20:34:42 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+dir_name="build/"
+dir_name=$dir_name$(basename $(pwd))-
+dir_name=$dir_name$(date +"%Y-%m-%d_%H-%M-%S.%s")
+mkdir -p $dir_name
+cp -R dist $dir_name
+cp ./manifest.json $dir_name
+zip -r "$dir_name.zip" $dir_name
